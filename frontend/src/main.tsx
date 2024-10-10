d8d27fb46a8c9f84b139f4054e673faa5d9b560d
Thainan Luiz <thainandj@hotmail.com>
2024-10-10 14:30:51 -0300

    feat(homepage): implement home screen

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,10 +3,12 @@ import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 
+import Home from "./pages/Home";
+
 const router = createBrowserRouter([
 	{
 		path: "/",
-		element: <></>,
+		element: <Home />,
 	},
 ]);
 
