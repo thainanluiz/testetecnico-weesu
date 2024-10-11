48d79232afc99ed823d27d9050239a5282cd8ee6
Thainan Luiz <thainandj@hotmail.com>
2024-10-11 19:21:00 -0300

    refactor: remove useless app controller and service
    refactor: configure main.ts (implement swagger, api versioning, api prefix and validation pipe)

--- backend/api-gateway/src/main.ts
+++ backend/api-gateway/src/main.ts
@@ -1,8 +1,50 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import {
+	BadRequestException,
+	ValidationPipe,
+	VersioningType,
+} from "@nestjs/common";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+	const app = await NestFactory.create(AppModule);
+
+	app.setGlobalPrefix("api");
+
+	app.enableVersioning({
+		type: VersioningType.URI,
+	});
+
+	app.useGlobalPipes(
+		new ValidationPipe({
+			whitelist: true,
+			transform: true,
+			exceptionFactory(errors) {
+				return new BadRequestException({
+					error_code: "BAD_REQUEST",
+					error_description: "Invalid request body",
+					errors: errors.map((error) => ({
+						field: error.property,
+						constraints: error.constraints,
+					})),
+				});
+			},
+		}),
+	);
+
+	const config = new DocumentBuilder()
+		.setTitle("Weesu API Gateway")
+		.setDescription("The Weesu Test Interview API Gateway")
+		.setVersion("1.0")
+		.build();
+
+	const document = SwaggerModule.createDocument(app, config);
+
+	SwaggerModule.setup("docs", app, document, {
+		jsonDocumentUrl: "/docs-json",
+	});
+
+	await app.listen(3000);
 }
 bootstrap();
--- backend/microservice/mercadolivre/src/main.ts
+++ backend/microservice/mercadolivre/src/main.ts
@@ -1,8 +1,26 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+	const app = await NestFactory.create(AppModule);
+
+	app.useGlobalPipes(
+		new ValidationPipe({
+			whitelist: true,
+			transform: true,
+			exceptionFactory(errors) {
+				return new BadRequestException({
+					error_code: "INVALID_DATA",
+					error_description: "Validation failed",
+					errors: errors.map((error) => ({
+						field: error.property,
+						constraints: error.constraints,
+					})),
+				});
+			},
+		}),
+	);
+
+	await app.listen();
 }
 bootstrap();
